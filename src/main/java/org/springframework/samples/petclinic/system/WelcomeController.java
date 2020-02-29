6db1305d938bf401348d9bcb8a2310c9e7828a9a
asomensari <asomensari@newrelic.com>
2020-02-28 23:15:56 -0300

    Log In Context Changes

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,29 +1,40 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.samples.petclinic.system;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.newrelic.api.agent.NewRelic;
 
 @Controller
 class WelcomeController {
 
+	// Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
 	@GetMapping("/")
 	public String welcome() {
+
+		NewRelic.addCustomParameter("user", "joedoe");
+		NewRelic.addCustomParameter("randomVal", Math.random() * 100);
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("userId", "testUser");
+		params.put("another", "value");
+		params.put("number", 123);
+
+		NewRelic.getAgent().getInsights().recordCustomEvent("MyCustomEvent", params);
+
+		NewRelic.noticeError("Expected Error", params, true);
+
+		logger.debug("**************************************************** welcome debug log");
+		logger.info("**************************************************** welcome debug log");
+		logger.warn("**************************************************** welcome debug log");
+
 		return "welcome";
 	}
 
